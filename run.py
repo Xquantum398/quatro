57f051999b5e9790303690a0b5a27f4001939b3b
Xquantum398 <inpinitypinity@gmail.com>
2026-01-13 00:10:38 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
-from mediaflow_proxy.main import app as mediaflow_app  # Import mediaflow app
-import httpx
-import re
-import string
+from mediaflow_proxy.main import app as mediaflow_app
 
-# Initialize the main FastAPI application
+# Ana uygulamayı başlat
 main_app = FastAPI()
 
-# Manually add only non-static routes from mediaflow_app
+# mediaflow_app içindeki route'ları ekle ("/" hariç)
 for route in mediaflow_app.routes:
-    if route.path != "/":  # Exclude the static file path
+    if route.path != "/":
         main_app.router.routes.append(route)
 
-# Run the main app
+# Docker CMD ile çalıştırıldığı için if __name__ == "__main__": bloğu opsiyonel
+# Ama geliştirme için istersen port 8888 ile çalıştırabilirsin
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(main_app, host="0.0.0.0", port=7860)
+    uvicorn.run(main_app, host="0.0.0.0", port=8888)
+
